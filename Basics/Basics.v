13cb6fd872e2a1b63ce70d436632f599bfa6382d
熊鑫 <1781189926@qq.com>
2018-10-06 15:58:01 +0800

    commit

--- Basics/Basics.v
+++ Basics/Basics.v
@@ -171,4 +171,106 @@ Module NatPlayground2.
       | S n' => S ( plus n' m )
     end.
   Compute (plus 3 2).
+
+  Fixpoint mult (n m : nat) :=
+    match n with
+      | O => O
+      | S n' => plus m ( mult n' m )
+    end.
+
+  Fixpoint minus (n m : nat) :=
+    match n, m with
+      | O, _ => O
+      | S _, O => n
+      | S n', S m' => minus n' m'
+    end.
+  Example test_mult1: (mult 3 3) = 9.
+  Proof. simpl. reflexivity. Qed.
+  Example test_minus1: (minus 9 3) = 6.
+  Proof. simpl. reflexivity. Qed.
+  
 End NatPlayground2.
+
+Fixpoint exp (base power : nat) : nat :=
+    match power with
+      | O => S O (* 任意自然数的一次方等于 1 *)
+      | S p => mult base (exp base p)
+    end.
+
+Fixpoint factorial (n : nat) : nat :=
+  match n with
+    | O => S O
+    | S n' => mult n (factorial n')
+  end.
+
+Example test_factorial1: (factorial 5) = (mult 10 12).
+Proof. simpl. reflexivity. Qed.
+
+Notation "x + y" :=
+  (plus x y)
+  (at level 50, left associativity)
+  : nat_scope.
+
+Notation "x - y" :=
+  (minus x y)
+  (at level 50, left associativity)
+  : nat_scope.
+
+Notation "x * y" :=
+  (mult x y)
+  (at level 40, left associativity)
+  : nat_scope.
+
+Check ( (0 + 1) + 1 ).
+
+Fixpoint beq_nat (n m : nat) : bool :=
+  match n with
+    | O => match m with
+            | O => true
+            | S m' => false
+            end
+    | S n' => match m with
+              | O => false
+              | S m' => beq_nat n' m'
+              end
+    end.
+
+Compute ( beq_nat 12 12 ).
+
+Fixpoint leb (n m : nat) : bool :=
+  match n with
+  | O => true
+  | S n' => match m with
+            | O => false
+            | S m' => leb n' m'
+            end
+  end.
+
+Example test_leb1: (leb 2 2) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_leb2: (leb 2 4) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_leb3: (leb 4 2) = false.
+Proof. simpl. reflexivity. Qed.
+
+(*Exercise: 1 star (blt nat)*)
+Fixpoint blt_nat (n m : nat) : bool :=
+  match n, m with
+  | O, O => false
+  | O, _ => true
+  | _, O => false
+  | S n', S m' => blt_nat n' m'
+  end. 
+
+Example test_blt_nat1: (blt_nat 2 2) = false.
+Proof. simpl. reflexivity. Qed.
+Example test_blt_nat2: (blt_nat 2 4) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_blt_nat3: (blt_nat 4 2) = false.
+Proof. simpl. reflexivity. Qed.
+
+Theorem plus_0_n: forall n : nat, O + n = n.
+Proof.
+  intros n. simpl. reflexivity. Qed.
+  
+  
